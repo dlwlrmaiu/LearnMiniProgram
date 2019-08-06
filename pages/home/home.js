dc65e2e3b19a2855c1047c9f1a00343b7e5ba31c
dlwlrmaiu <1917688982@qq.com>
2019-08-06 20:03:16 +0800

    展示弹窗和分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,66 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast: function() {
+    wx.showToast({
+      title: '加载中ing',
+      duration: 3000,
+      icon: 'loading',
+      // image: '../../aseest/image/p.png'
+      mask: true,
+      success: function() {
+        console.log('展示弹窗成功')
+      },
+      fail: function() {
+        console.log("展示弹窗失败")
+      },
+      complete: function() {
+        console.log('完成函数的调用')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal: function() {
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容,哈哈哈',
+      // showCancel: false,
+      cancelText: '退出',
+      cancelColor: 'red',
+      success: function(res) {
+        console.log(res)
+        if(res.cancel) {
+          console.log('用户点击了取消按钮')
+        }
+        if (res.confirm) {
+          console.log('用户点击了确定按钮')
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading:function() {
+    wx.showLoading({
+      title: '加载ing',
+      mask: true
+    })
+    setTimeout(() => {
+      // 必须手动hideLoading才会让loading消失
+      wx.hideLoading()
+    },1000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowActionSheet: function() {
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      itemColor: 'red',
+      success: function(res) {
+        console.log(res)
+        switch(res.tapIndex){}
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function (options) {
+    return {
+      title: '你好啊,dlwlrma',
+      path: '/pages/about/about',
+      imageUrl: 'http://s3.mogucdn.com/mlcdn/c45406/170915_0a93207ci28kelh617k4hh62l65lb_640x960.jpg'
+    }
   }
 })
